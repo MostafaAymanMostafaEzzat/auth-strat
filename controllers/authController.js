7faf5d870b3d723f600b68c18b1db74c561b6bbb
es-Moustafa.Ezzat2026 <es-moustafa.ezzat2026@alexu.edu.eg>
2024-02-08 19:08:12 +0200

    bugggggg

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,9 +18,9 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const user = await User.create({ name, email, password, role });
-  const tokenUser = createTokenUser(user);
+  
   const verificationToken = cyrpto.randomBytes(40).toString('hex');
+  const user = await User.create({ name, email, password, role,verificationToken });
   const origin = 'https://react-3t4g.onrender.com';
 
 
