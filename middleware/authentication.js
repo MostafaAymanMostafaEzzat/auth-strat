c42bf386c0d9b7d53fbc0d8c6a7db8d0d6bf2d5c
es-Moustafa.Ezzat2026 <es-moustafa.ezzat2026@alexu.edu.eg>
2024-02-08 23:57:51 +0200

    aaaa

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,14 +3,13 @@ const { isTokenValid,attachCookiesToResponse } = require('../utils');
 const Token =require('../models/Tokens')
 
 const authenticateUser = async (req, res, next) => {
-  const accessToken = req.signedCookies.accessToken;
-  const refreshToken = req.signedCookies.refreshToken;
+  const { refreshToken, accessToken } = req.signedCookies;
 
   try {
     if(accessToken){
       const payload = isTokenValid({ accessToken });
       req.user = payload.user;
-      next();
+      return next();
     }
     const payload = isTokenValid({ refreshToken });
     const existingToken =await Token.findOne({
